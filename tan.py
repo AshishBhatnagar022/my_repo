83266f6711b1046ae407e81d62e009ba79daca04
Ashish <bhatnagarashish022@gmail.com>
2021-05-16 01:12:06 +0530

    done

--- tan.py
+++ tan.py
@@ -1,6 +1,6 @@
 
 sin square + cos sqare =1
 hello from myFeature
-changing the code1 in tan45
-changing the code in tan44
-in kaju branch
+changing the code1 in tan455
+changing the code in tan444
+in kaju branch1
